469397873a9cdfc6ccd7dfa452cf2ddfeef785c3
Giona Righini <grighini@alteanet.it>
2024-02-25 20:01:50 +0100

    query for test

--- stats.sql
+++ stats.sql
@@ -1,11 +1,11 @@
 -- day week
 SELECT
 	date(timestamp) AS data,
-	round(avg(bm_voltage), 4) bm_V,
-	round(avg(b1_voltage), 4) b1_V,
-	round(avg(b2_voltage), 4) b2_V,
-	round(sum(`b1_current` * `coeff`), 4) AS b1_Ah,
-	round(sum(`b2_current` * `coeff`), 4) AS b2_Ah
+	round(avg(bm_voltage), 2) bmV,
+	round(avg(b1_voltage), 2) b1V,
+	round(avg(b2_voltage), 2) b2V,
+	round(sum(`b1_current` * `coeff`), 2) AS b1Ah,
+	round(sum(`b2_current` * `coeff`), 2) AS b2Ah
 FROM
 	`battery-snaps`
 WHERE
@@ -16,11 +16,11 @@ GROUP BY
 -- realtime
 SELECT
 	`timestamp`,
-	round(bm_voltage, 3) AS bm_V,
-	round(b1_voltage, 3) AS b1_V,
-	round(b2_voltage, 3) AS b2_V,
-	round(`b1_current`, 3) AS b1_A,
-	round(`b2_current`, 3) AS b2_A
+	round(bm_voltage, 2) AS bmV,
+	round(b1_voltage, 2) AS b1V,
+	round(b2_voltage, 2) AS b2V,
+	round(`b1_current`, 2) AS b1A,
+	round(`b2_current`, 2) AS b2A
 FROM
 	`battery-snaps`
 WHERE
@@ -28,3 +28,19 @@ WHERE
 ORDER BY
 	id DESC
 LIMIT 500;
+
+
+-- for testing
+SELECT
+	date(timestamp) AS data,
+	round(avg(bm_voltage), 4) bmV,
+	round(avg(b1_voltage), 4) b1V,
+	round(avg(b2_voltage), 4) b2V,
+	round(avg(`b1_current`), 4) AS b1A,
+	round(avg(`b2_current`), 4) AS b2A
+FROM
+	`battery-snaps`
+WHERE
+	date(timestamp)> (NOW() - INTERVAL 7 DAY)
+GROUP BY
+	data;
