546e3a90371d5977ae14b8a5af1904b4cb93574a
Giona Righini <grighini@alteanet.it>
2024-03-09 20:23:47 +0100

    lombardo

--- src/camera.js
+++ src/camera.js
@@ -21,7 +21,9 @@ module.exports = {
       if (movement) {
         const fileName = `./camera/${new Date().toISOString()}.jpg`;
 
-        execSync(`libcamera-still -o ${fileName} --width 2028 --height 1520`);
+        execSync(
+          `libcamera-still -o ${fileName} --width 2028 --height 1520 --shutter 5000000 --gain 1 --immediate`
+        );
 
         onMovement(fileName);
       }
@@ -36,7 +38,9 @@ module.exports = {
   picture: () => {
     const fileName = `./camera/${new Date().toISOString()}.jpg`;
 
-    execSync(`libcamera-still -o ${fileName} --width 2028 --height 1520`);
+    execSync(
+      `libcamera-still -o ${fileName} --width 2028 --height 1520 --shutter 5000000 --gain 1 --immediate`
+    );
 
     return fileName;
   },
