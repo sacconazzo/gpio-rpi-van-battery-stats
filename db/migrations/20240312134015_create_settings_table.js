5e3a78dad4d5f59b762dc66a563b687b072b6269
Giona Righini <grighini@alteanet.it>
2024-03-12 13:55:56 +0100

    fix settings

--- db/migrations/20240312134015_create_settings_table.js
+++ db/migrations/20240312134015_create_settings_table.js
@@ -2,7 +2,7 @@ exports.up = (knex) => {
   return knex.schema.createTable("settings", (table) => {
     table.increments("id").primary();
     table.string("key", 20).unique().notNullable();
-    table.float("value", 10, 7).notNullable();
+    table.string("value", 20).notNullable();
     table.string("name", 20);
     table.string("notes");
   });
