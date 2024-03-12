2972713efb12bc70d702358dc8b593dd02497c7e
Giona Righini <grighini@alteanet.it>
2024-03-12 13:48:22 +0100

    init settings table

---
+++ db/migrations/20240312134015_create_settings_table.js
@@ -0,0 +1,11 @@
+exports.up = (knex) => {
+  return knex.schema.createTable("settings", (table) => {
+    table.increments("id").primary();
+    table.string("key", 20).unique().notNullable();
+    table.float("value", 10, 7).notNullable();
+    table.string("name", 20);
+    table.string("notes");
+  });
+};
+
+exports.down = (knex) => knex.schema.dropTableIfExists("settings");
