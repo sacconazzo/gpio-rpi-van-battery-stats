5914d800fb8a287a8206493ecff00f978d1434f0
Giona Righini <grighini@alteanet.it>
2024-03-18 20:16:45 +0100

    cron script

--- start.sh
+++ start.sh
@@ -4,10 +4,10 @@ cd /home/pi/gpio-rpi-van-battery-stats
 
 COLLECT=$(pgrep -f collect.py)
 if [ -z "$COLLECT" ]; then
-    yarn collect &
+    sudo yarn collect &
 fi
 
 MANAGER=$(pgrep -f manager.js)
 if [ -z "$MANAGER" ]; then
-    yarn manager &
+    sudo yarn manager &
 fi
