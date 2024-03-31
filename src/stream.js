3ea3a91725c178359ab5cb95e10efca64e9d1a50
sacconazzo <sacconazzo@gmail.com>
2024-03-31 19:42:17 +0200

    init stream

---
+++ src/stream.js
@@ -0,0 +1,48 @@
+const http = require('http');
+const fs = require('fs');
+const RaspiCam = require('raspicam');
+const io = require('socket.io')(http);
+
+// Configura la fotocamera
+const camera = new RaspiCam({
+  mode: 'photo', // Imposta la modalità su 'photo' per catturare immagini
+  output: 'latest.jpg', // Nome del file di output
+  encoding: 'jpg', // Formato di encoding dell'immagine
+  timeout: 0 // Nessun timeout, continua a scattare foto
+});
+
+// Avvia il server HTTP
+const server = http.createServer((req, res) => {
+  fs.readFile(__dirname + '/index.html', (err, data) => {
+    if (err) {
+      res.writeHead(500);
+      return res.end('Error loading index.html');
+    }
+    res.writeHead(200);
+    res.end(data);
+  });
+});
+
+// Avvia il server di streaming
+io.on('connection', (socket) => {
+  console.log('Client connected');
+  camera.start(); // Avvia la cattura delle immagini
+  camera.on('read', (err, timestamp, filename) => {
+    if (err) {
+      console.log(err);
+    } else {
+      fs.readFile(filename, (err, data) => {
+        if (err) {
+          console.log(err);
+        } else {
+          socket.emit('image', data); // Invia l'immagine al client
+        }
+      });
+    }
+  });
+});
+
+// Ascolta sulla porta 3000
+server.listen(3000, () => {
+  console.log('Server running on port 3000');
+});
