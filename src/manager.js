6a0eb1e51b2b533e110fd5a1713fe5e1186c4e2d
sacconazzo <sacconazzo@gmail.com>
2024-05-04 14:06:27 +0200

    calibrate +

--- src/manager.js
+++ src/manager.js
@@ -7,6 +7,7 @@ const Gpio = require("pigpio").Gpio;
 const { exec, execSync } = require("child_process");
 const db = require("./db");
 const telegram = require("./telegram");
+const recalibrateCurrentSensor = require("./calibrate");
 
 const apiToken = process.env.API_TOKEN;
 
@@ -171,59 +172,7 @@ if (process.env.ENABLE_BUTTONS === "true") {
   setInterval(clearLedButtons, 100);
 }
 
-// CRON - RICALIBRATE SENSOR
-const recalibrateCurrentSensor = async ({ force = false }) => {
-  const [[settings]] = await db.raw(
-    `select\
-    count(*) as snaps,
-    truncate(avg(a.ch5), 4) as OFFSET_A1,\
-    truncate(avg(a.ch6), 4) as OFFSET_A2,\
-    avg(b.b1_current) as A1,\
-    avg(b.b2_current) as A2,\
-    round(avg(b.temperature), 2) as TEMPERATURE\
-    from\
-      \`battery-snaps\` b\
-    join \`adc-snaps\` a on\
-      a.timestamp = b.timestamp\
-    where\
-      a.timestamp > (NOW() - INTERVAL ${force ? "3 MINUTE" : "1 HOUR"})\
-      ${
-        force
-          ? ";"
-          : "and b1_current < 0.3\
-             and b1_current > -0.3\
-             and b2_current < 0.3\
-             and b2_current > -0.3\
-             HAVING\
-             snaps > 10;"
-      }`
-  );
-
-  console.log(`Current sensor recalibrate: ${JSON.stringify(settings)}`);
-  if (!settings) return;
-
-  await db("settings")
-    .update({ value: String(settings.OFFSET_A1) })
-    .where({ key: "OFFSET_A1" });
-  await db("settings")
-    .update({ value: String(settings.OFFSET_A2) })
-    .where({ key: "OFFSET_A2" });
-  await db("settings")
-    .update({
-      value: String(settings.TEMPERATURE),
-      notes: new Date().toISOString(),
-    })
-    .where({ key: "TREF_A1" });
-  await db("settings")
-    .update({
-      value: String(settings.TEMPERATURE),
-      notes: new Date().toISOString(),
-    })
-    .where({ key: "TREF_A2" });
-
-  return settings;
-};
-
+// CRON - re-calibrating
 cron.schedule(recalibrateInterval, recalibrateCurrentSensor);
 
 // LOGGER
--- src/telegram.js
+++ src/telegram.js
@@ -43,7 +43,9 @@ bot.use(isAuthorized);
 
 bot.command("calibrate", async (ctx) => {
   try {
-    const setup = await events.onCalibrateRequest({ force: true });
+    await ctx.reply(`Current sensor recalibrate: please wait...`);
+
+    const setup = await events.onCalibrateRequest();
 
     await ctx.reply(
       `Current sensor recalibrate:\n${
