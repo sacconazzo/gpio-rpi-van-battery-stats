124a43dd85f422a1462ca2bbf57b6e5f9476fc1d
Giona Righini <grighini@alteanet.it>
2024-06-07 18:06:32 +0200

    try openai

--- src/ai.js
+++ src/ai.js
@@ -1,14 +1,30 @@
-const OpenAI = require("openai");
+// index.js
+require("dotenv").config();
+const OpenAIApi = require("openai");
 
-const openai = new OpenAI();
+// Carica la chiave API dalle variabili di ambiente
+const apiKey = process.env.OPENAI_API_KEY;
+
+const openai = new OpenAIApi.OpenAI({ key: apiKey });
 
 async function main() {
   const completion = await openai.chat.completions.create({
-    messages: [{ role: "system", content: "You are a helpful assistant." }],
+    messages: [
+      { role: "system", content: "You are a helpful assistant." },
+      {
+        role: "user",
+        content: "Chi ha vinto il campionato di formula1 nel 1998?",
+      },
+      // {
+      //   role: "assistant",
+      //   content: "The Los Angeles Dodgers won the World Series in 2020.",
+      // },
+      // { role: "user", content: "Where was it played?" },
+    ],
+    max_tokens: 60,
     model: "gpt-3.5-turbo",
   });
 
   console.log(completion.choices[0]);
 }
-
 main();
