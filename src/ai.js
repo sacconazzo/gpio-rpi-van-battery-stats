3fabffdf0c3f21f2f7aa2126b214979501e1cf90
sacconazzo <sacconazzo@gmail.com>
2024-06-08 12:49:29 +0200

    calibration ai

--- src/ai.js
+++ src/ai.js
@@ -30,12 +30,12 @@ async function main() {
       {
         role: "user",
         content:
-          "Ti fornirò un array in formato stringificato, avrà 2 campi, ch5 e ch6, rappresentano il valore grezzo dei sensori di corrente. " +
+          "Ti fornirò un array in formato stringificato, avrà 2 campi, ch5 e ch6, rappresentano il valore grezzo dei sensori di corrente, uno snapshot ogni 20 secondi. l'ultimo elemento il più recente. " +
           "In linea teorica un valore vicino a 0.5 indica che non c'è flusso di corrente, mentre spostandosi da quel valore potrebbe esserci assorbimento o ricarica. " +
           "I sensori sono collegati a 2 batterie al litio in un van e a pannello solare che potrebbe caricare o meno dipende dal sole e dallo stato di ricarica, le batterie sono anche collegati ai servizi del van. " +
           "Tendenzialmente i sensori subiscono un offset dello 0 e quindi in fase di riposo, senza assorbimento o carica, il valore che lo rappresenta si scosta. " +
           "Ho necessità che tu mi analizzi l'array di valori e mi suggerisci il valore attuale di 0 ricalibrato per entrambi i 2 set ch5 e ch6. " +
-          "la risposta deve essere 2 valori numerici semplici separati da virgola e nient'altro che rappresentino ch5 e ch6. Se non sei in grado di rispondere restituisci con stringa vuota.",
+          "la risposta deve essere 2 valori numerici semplici separati da virgola e nient'altro che rappresentino ch5 e ch6. Se non sei in grado di rispondere perchè non hai dati sufficienti o validi restituisci una stringa vuota.",
       },
       {
         role: "assistant",
@@ -43,7 +43,7 @@ async function main() {
       },
     ],
     max_tokens: 60,
-    model: "gpt-4-turbo",
+    model: "gpt-4o",
   });
 
   console.log(completion.choices[0]);
