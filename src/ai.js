1046583f961fff169b05bf5d259d2e738298a4da
Giona Righini <grighini@alteanet.it>
2024-06-08 13:13:53 +0200

    calibrate ai

--- src/ai.js
+++ src/ai.js
@@ -1,17 +1,13 @@
-// index.js
-require("dotenv").config();
 const db = require("./db");
 const OpenAIApi = require("openai");
 
-// Carica la chiave API dalle variabili di ambiente
 const apiKey = process.env.OPENAI_API_KEY;
-
 const openai = new OpenAIApi.OpenAI({ key: apiKey });
 
-async function main() {
+const aai = async () => {
   const [signals] = await db.raw(
     `SELECT\
-      ch5, ch6\
+      ch5, ch6, ch7\
     FROM\
     \`adc-snaps\`\
     WHERE\
@@ -25,17 +21,19 @@ async function main() {
       {
         role: "system",
         content:
-          "Sei un assistente che deve fornire dati in formato non testiale ma in un formato adatto ad un elaboratore. Ad ogni domanda rispondi solamente con i dettagli richiesti senza aggiungere altro",
+          "Fornisci dati in formato adatto ad un elaboratore. Ad ogni domanda rispondi solamente con i dettagli richiesti senza aggiungere altro",
       },
       {
         role: "user",
         content:
-          "Ti fornirò un array in formato stringificato, avrà 2 campi, ch5 e ch6, rappresentano il valore grezzo dei sensori di corrente, uno snapshot ogni 20 secondi. l'ultimo elemento il più recente. " +
+          "Ti fornirò un array in formato stringificato, avrà 2 campi, ch5 e ch6, rappresentano il valore grezzo dei sensori di corrente. " +
+          "Il campo ch7 è solo un dettaglio aggiuntivo, il valore grazzo da un termistore per quel determinato snapshot, la temperatura potrebbe influire con i valori dei sensori di corrente. " +
+          "Uno snapshot ogni 20 secondi. l'ultimo elemento il più recente. " +
           "In linea teorica un valore vicino a 0.5 indica che non c'è flusso di corrente, mentre spostandosi da quel valore potrebbe esserci assorbimento o ricarica. " +
           "I sensori sono collegati a 2 batterie al litio in un van e a pannello solare che potrebbe caricare o meno dipende dal sole e dallo stato di ricarica, le batterie sono anche collegati ai servizi del van. " +
           "Tendenzialmente i sensori subiscono un offset dello 0 e quindi in fase di riposo, senza assorbimento o carica, il valore che lo rappresenta si scosta. " +
           "Ho necessità che tu mi analizzi l'array di valori e mi suggerisci il valore attuale di 0 ricalibrato per entrambi i 2 set ch5 e ch6. " +
-          "la risposta deve essere 2 valori numerici semplici separati da virgola e nient'altro che rappresentino ch5 e ch6. Se non sei in grado di rispondere perchè non hai dati sufficienti o validi restituisci una stringa vuota.",
+          "la risposta deve essere 2 valori numerici semplici separati da virgola e nient'altro che rappresentino ch5 e ch6. Se non ricevi i valori di riferimento o non sei in grado di rispondere perchè non hai dati sufficienti, rispondi con 0",
       },
       {
         role: "assistant",
@@ -46,6 +44,18 @@ async function main() {
     model: "gpt-4o",
   });
 
-  console.log(completion.choices[0]);
-}
-main();
+  const calibration =
+    completion.choices[0]?.message?.content &&
+    completion.choices[0].message.content.includes(",")
+      ? completion.choices[0].message.content.split(",")
+      : [];
+
+  return {
+    a1: calibration[0],
+    a2: calibration[1],
+  };
+};
+
+module.exports = aai;
+
+// aai().then((s) => console.log(s));
