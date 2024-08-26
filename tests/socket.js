af883b549b187d7ea933f575ec136466efec7164
Giona Righini <grighini@alteanet.it>
2024-08-26 23:42:54 +0200

    test socket

--- tests/socket.js
+++ tests/socket.js
@@ -1,19 +1,11 @@
-const http = require("http");
-const { Server } = require("socket.io");
-const server = http.createServer();
-const io = new Server(server);
+const io = require("socket.io-client");
 
-const ioc = require("socket.io-client");
-
-server.listen(3000, () => {});
-setTimeout(() => io.emit("test", 12), 2000);
-
-const socket = ioc("http://localhost:3000");
+const socket = io("http://10.8.0.5:3000");
 
 socket.on("connect", () => {
   console.log("connected!");
 });
 
-socket.on("test", (msg) => {
+socket.on("data", (msg) => {
   console.log("Received:", msg);
 });
